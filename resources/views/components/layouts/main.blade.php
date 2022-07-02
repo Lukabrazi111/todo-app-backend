721f4a3224e6c6dfa5a50f8d18242fc4bf455e16
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2022-07-03 03:30:58 +0400

    chore: uncomment some lines

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    {{--    <title>{{ config('app.name') }}</title>--}}
+    <title>{{ config('app.name') }}</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
     <!-- Alpine js -->
