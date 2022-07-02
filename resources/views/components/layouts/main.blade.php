1fe380a726e4754b73e42e3f68ed8923a692f287
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2022-07-03 01:22:55 +0400

    feat: create dropdown

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -6,8 +6,13 @@
     <title>{{ config('app.name') }}</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+    <!-- Alpine js -->
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <style>[x-cloak] {
+            display: none !important
+        }</style>
 
 </head>
 <body class="antialiased bg-orange-100">
@@ -26,8 +31,38 @@
             </div>
             @if(auth()->check())
                 <ul class="flex items-center space-x-10">
-                    <li>
-                        <a href="{{ route('user.logout') }}" class="px-5 py-4 hover:underline text-lg">Logout</a>
+                    <li
+                        x-cloak
+                        x-data="{ open: false }"
+                        class="flex items-center space-x-4 relative">
+                        <span>{{ auth()->user()->username }}</span>
+                        <div>
+                            <img src="{{ asset('avatars/default-avatar.png') }}" alt="default-avatar"
+                                 class="w-10 h-10 rounded-full">
+                        </div>
+                        <button
+                            @click="open = !open"
+                            class="py-3 px-2">
+                            <span>
+                                <svg xmlns="http://www.w3.org/2000/svg" :class="open ? 'rotate-180' : ''"
+                                     class="h-6 w-6 transition-all" fill="none"
+                                     viewBox="0 0 24 24"
+                                     stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7"/>
+                                </svg>
+                            </span>
+                        </button>
+                        <div
+                            x-show="open"
+                            x-transition.origin.top.duration.200ms
+                            class="bg-white text-black w-40 absolute top-16 z-20 border border-gray-400 flex flex-col rounded">
+                            <a href="#"
+                               class="px-2 py-2 bg-gray-100 text-gray-900 border-b border-gray-400 hover:bg-gray-300">Account</a>
+                            <a href="#"
+                               class="px-2 py-2 bg-gray-100 text-gray-900 border-b border-gray-400 hover:bg-gray-300">Profile</a>
+                            <a href="{{ route('user.logout') }}"
+                               class="px-2 py-2 bg-gray-100 text-gray-900 border-b hover:bg-gray-300">Logout</a>
+                        </div>
                     </li>
                 </ul>
             @else
